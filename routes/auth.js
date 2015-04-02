a02b6699a277312300e954517569c526e1aa16da
Michel Matthias Rösler <michel.roesler@fokus.fraunhofer.de>
2015-04-02 04:07:07 +0200

    local authentication strategy

--- routes/auth.js
+++ routes/auth.js
@@ -5,24 +5,36 @@
 var express = require('express');
 var router = express.Router();
 
-router.route('/signup')
-    .get(function(req,res){
-        // check cookie for saved credentials
-    })
-    .post(function(req,res){
-        // check if username is taken
-        // != register user
+module.exports = function(passport){
+
+    //sends successful login state back to angular
+    router.get('/success', function(req, res){
+        res.send({state: 'success', user: req.user ? req.user : null});
     });
 
-router.route('/signin')
-    .post(function(req,res){
-        // check creadentials
+    //sends failure login state back to angular
+    router.get('/failure', function(req, res){
+        res.send({state: 'failure', user: null, message: "Invalid username or password"});
     });
 
-router.route('/logout')
-    .get(function(req,res){
-        // redirect to landing page
+    //log in
+    router.post('/login', passport.authenticate('login', {
+        successRedirect: '/auth/success',
+        failureRedirect: '/auth/failure'
+    }));
+
+    //sign up
+    router.post('/signup', passport.authenticate('signup', {
+        successRedirect: '/auth/success',
+        failureRedirect: '/auth/failure'
+    }));
+
+    //log out
+    router.get('/signout', function(req, res) {
+        req.logout();
+        res.redirect('/');
     });
 
+    return router;
 
-module.exports = router;
+}
